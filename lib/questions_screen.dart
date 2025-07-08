7c712a858aba83c7e9c15f0db25121b39869996e
KARAN NANA KATAKDHOND <karankatakdhond23@gmail.com>
2025-07-08 18:17:40 +0530

    added a onSelectAnswer() Function in quiz_screen.dart

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({required this.onSelectAnswer,super.key});
+  final void Function(String answer) onSelectAnswer;
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreen();
@@ -14,6 +16,7 @@ class _QuestionsScreen extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
   answerQuestion(){
+    Widget.onSelectAnswer(String selectedAnswers)
     //as we also want to rebuild the widget after 
     //updation of the currentQuestionIndex we use setState((){})
     setState(() {
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,40 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'start_screen.dart';
-import 'questions_screen.dart';
 
-class Quiz extends StatefulWidget{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
   @override
-  State<Quiz> createState(){
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
-class _QuizState extends State<Quiz>{
-//Widget? activeScreen;
-var activeScreen = 'start-screen';
-@override
-// void initState(){
-//   activeScreen = StartScreen(switchScreen);
-//   super.initState();
-// }
-@override
-void switchScreen(){
+class _QuizState extends State<Quiz> {
+  //Widget? activeScreen;
+  final List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+  @override
+  // void initState(){
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+  @override
+  void switchScreen() {
     setState(() {
       activeScreen = 'questions-screen';
       //activeScreen = const QuestionsScreen();
     });
   }
 
-  @override 
-  Widget build(context){
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+  }
+
+  @override
+  Widget build(context) {
     // var screenWidget = activeScreen == 'start=screen'
     //     ? StartScreen(switchScreen)
     //     : const QuestionsScreen();
     Widget screenWidget = StartScreen(switchScreen);
-    if(activeScreen=='questions-screen'){
-      screenWidget = const QuestionsScreen();
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chooseAnswer);
     }
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -46,11 +50,11 @@ void switchScreen(){
                 Color.fromARGB(255, 78, 13, 151),
                 Color.fromARGB(255, 107, 15, 168),
               ],
-              begin:Alignment.topLeft,
-              end:Alignment.bottomRight,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
-          child:screenWidget,
+          child: screenWidget,
           //child: activeScreen,
         ),
         //backgroundColor:const Color.fromARGB(255, 122, 16, 221),
