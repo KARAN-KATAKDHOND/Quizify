30324e7c673f5c554279ccf18442a4e4cc0d4d95
KARAN NANA KATAKDHOND <karankatakdhond23@gmail.com>
2025-07-10 23:39:52 +0530

    APK build and added initial of developer me.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'start_screen.dart';
 import 'quiz.dart';
 import 'questions_screen.dart';
-
+// Developed by K.K. (Karan Katakdhond)
+// Date: 2025-07-10
 void main() {
   runApp(Quiz());
 }
